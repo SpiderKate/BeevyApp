7d39bf54d4c23d3671e8a897f88f55fd060ae81e
SpiderKate <109092125+SpiderKate@users.noreply.github.com>
2025-06-10 19:44:34 +0200

    login, register, links
    basic login + register without putting the inputs anywhere, links through pages + adding classes for css

--- app.py
+++ app.py
@@ -2,15 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    if request.method == 'POST':
+        print('post')
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
         return redirect(url_for("index"))
-    return render_template("login.html")
+    else:
+        return render_template("login.html")
+    
+@app.route('/register', methods=['GET','POST'])
+def register():
+    return render_template("register.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)

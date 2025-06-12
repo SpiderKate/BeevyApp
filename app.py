4417df5928671a33f4e477e7dc0ccfe77a4e0c6e
SpiderKate <109092125+SpiderKate@users.noreply.github.com>
2025-06-12 15:02:39 +0200

    database connection
    zatím jen ukládá data do databáze

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bcrypt import bcrypt
+import sqlite3
+import sys
+
+print('some debug', file=sys.stderr)
 
 app = Flask(__name__)
 
@@ -12,12 +17,36 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        print('login_test')
         return redirect(url_for("index"))
+        
+          
     else:
         return render_template("login.html")
     
 @app.route('/register', methods=['GET','POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        surname = request.form['surname']
+        email = request.form['email']
+        dob = request.form['dob']
+        bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(bytes, salt)
+    #need to complete import to db and controle on login
+        try:
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users (username, password, name, surname, email, dob) VALUES (?, ?, ?, ?, ?, ?)", (username, hash, name, surname, email, dob))
+            conn.commit()
+            print('regoster_test')
+            conn.close()
+            return redirect(url_for("login"))
+        except sqlite3.IntegrityError:
+            return "User already exists!"
     return render_template("register.html")
     
 

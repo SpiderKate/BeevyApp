8d45ab03d8d96c51d9daf5df01e8f061a5ab6701
SpiderKate <109092125+SpiderKate@users.noreply.github.com>
2025-06-16 18:04:01 +0200

    comments

--- app.py
+++ app.py
@@ -14,17 +14,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    #bere input ze stranky
     if request.method == 'POST':
         usEm = request.form['username']
         password = request.form['password']
+        #heslo ze starnky => bytes
         user_bytes = password.encode('utf-8')
         try:
             conn = sqlite3.connect('users.db')
             cursor = conn.cursor()
+            #hleda heslo bud pro username ci email
             cursor.execute("SELECT password FROM users WHERE email=? OR username=?",(usEm, usEm))
+            #vysledek se popripadne ulozi sem
             result = cursor.fetchone()
+            #a kdyz to najde heslo k danému username ci email tak ho zkontroluje
             if result:
                 db_pass = result[0]
+                #kdyz je spravne posle uzivatele na userPage
                 if bcrypt.checkpw(user_bytes, db_pass):
                     return redirect(url_for("userPage"))
                 else:
@@ -40,6 +46,7 @@ def login():
     
 @app.route('/register', methods = ['GET','POST'])
 def register():
+    #bere input ze stranky
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -52,6 +59,8 @@ def register():
         password_bytes = password.encode('utf-8')
         salt = bcrypt.gensalt()
         hash = bcrypt.hashpw(password_bytes, salt)
+        
+        #zapsani do db pokud user neexistuje (username ci email)
         try:
             conn = sqlite3.connect('users.db')
             cursor = conn.cursor()
@@ -60,6 +69,7 @@ def register():
             cursor.execute("SELECT email FROM users WHERE email=?", (email,))
             existing_email = cursor.fetchone()
 
+            #vypisuje chyby (kdyz uz username/email je pouzit)
             reg_errors = []
             if existing_user:
                 print('username in use')
@@ -69,13 +79,12 @@ def register():
                 reg_errors.append("Email is already in use.")
             if reg_errors:
                 return render_template("register.html", reg_errors = reg_errors)
+            #kdyz nejsou zadne chyby tak input ze stranky zapise do db
             else:
                 cursor.execute("INSERT INTO users (username, password, name, surname, email, dob) VALUES (?, ?, ?, ?, ?, ?)", (username, hash, name, surname, email, dob))
                 conn.commit()
                 
                 return redirect(url_for("login"))
-        except sqlite3.IntegrityError:
-            return "User already exists!"
         finally:
             conn.close()
     return render_template("register.html")

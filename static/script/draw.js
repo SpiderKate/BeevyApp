9eda8f8952431cec961f245c8ccb54062c2776bb
SpiderKate <109092125+SpiderKate@users.noreply.github.com>
2025-07-17 19:13:01 +0200

    basic css

--- static/script/draw.js
+++ static/script/draw.js
@@ -1,22 +1,35 @@
 const canvas = document.getElementById('drawCanvas');
         const ctx = canvas.getContext('2d');
-        const socket = io();  // connect to WebSocket
+        const socket = io();  //pripoji se k WebSocket
 
         let drawing = false;
+        let lastX = 0;
+        let lastY = 0;
 
-        canvas.addEventListener('mousedown', () => drawing = true);
+        canvas.addEventListener('mousedown', (e) =>{
+            drawing = true;
+            lastX = e.offsetX;
+            lastY = e.offsetY;
+        });
         canvas.addEventListener('mouseup', () => drawing = false);
         canvas.addEventListener('mousemove', (e) => {
             if (!drawing) return;
             const x = e.offsetX;
             const y = e.offsetY;
-            socket.emit('draw', {x, y});
-            draw(x, y);
+            socket.emit('draw', {fromX: lastX, fromY: lastY, toX: x, toY: y});
+            draw(lastX, lastY, x, y);
+            lastX = x;
+            lastY = y;
         });
 
-        socket.on('draw', ({x, y}) => draw(x, y));
-
-        function draw(x, y) {
-            ctx.fillStyle = 'black';
-            ctx.fillRect(x, y, 2, 2);
+        socket.on('draw', ({fromX, fromY, toX, toY}) => {
+            draw(fromX, fromY, toX, toY);
+        });
+        function draw(fromX, fromY, toX, toY) {
+            ctx.strokeStyle = 'black';
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.moveTo(fromX, fromY);
+            ctx.lineTo(toX, toY);
+            ctx.stroke();
         }
